d99a7304ee16d2ae365fe3d8231c8c53f2784c64
claxx moldii <claxxmoldii@mac-mini.local>
2021-05-25 17:10:32 +0800

    initial commit

---
+++ layouts/components/pages/about.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let pic, body, body2, body3;
+</script>
+
+<section class="isAbout isGrid">
+  <img class="claxxWelder" src="/assets/images/{pic}" alt="welding minifig" />
+
+  <div class="content">
+    <p>{ body }</p>
+    <p>{ body2 }</p>
+    <p>{ body3 }</p>
+  </div>
+</section>
+
+<style>
+  .isAbout {
+    /*background-color: beige;*/
+  }
+
+  .isGrid {
+    grid-template-columns: 4fr 6fr ;
+  }
+
+  .claxxWelder {
+    width: 250px;
+    border:  6px hsla(0, 100%, 65%, .9) solid;
+    border-radius: 50%;
+  }
+</style>
---
+++ layouts/global/footer.svelte
@@ -0,0 +1,47 @@
+<script>
+  import { makeTitle } from '../scripts/make_title.svelte';
+  import FooterInfo from '../components/footerInfo.svelte';
+
+  export let allContent;
+</script>
+
+<footer class="isFlex">
+  <div>
+    <span>All content:</span>
+    {#each allContent as content}
+      <a href="{content.path}">{makeTitle(content.filename)}</a>
+    {/each}
+  </div>
+  <hr class="sep">
+  <FooterInfo />
+</footer>
+
+<style>
+  footer {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    /*align-items: center;*/
+    /*background-color: var(--base-dark);*/
+    /*margin-top: 100px;*/
+    padding-top:  1rem;
+    flex-shrink: 0;
+  }
+  span {
+    color: var(--primary);
+    font-weight: bold;
+  }
+  a {
+    /*color: white;*/
+    color: darkgrey;
+    text-decoration: none;
+    margin-left: 10px;
+  }
+
+  a:hover {
+    color: tomato;
+  }
+
+  .sep {
+  }
+</style>
